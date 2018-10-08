6c652e47d2e029febf67f79bf3b83d129ae068bb
Manuel <manniealfaro@gmail.com>
2018-10-08 14:36:47 +0200

    First app try

---
+++ browser/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+
+def index(request):
+	tienda = {'name':'Tienda numero uno'}
+
+	template_name = "browser/index.html"
+	context = {'tienda':tienda}
+
+	return render(request = request, template_name = template_name, context = context)

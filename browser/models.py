f8ee4869c0061467699986e4bb5104e3a68ef95c
joseca97 <arinero97@gmail.com>
2018-10-09 10:24:32 +0200

    First model created

--- browser/models.py
+++ browser/models.py
@@ -3,4 +3,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Cliente(models.Model):
+	id = models.IntegerField(primary_key=True)
+
+	class Meta:
+		verbose_name = "Cliente"
+		verbose_name_plural = "Clientes"
+
+	def __str__(self):
+		pass
+    
--- browser/views.py
+++ browser/views.py
@@ -4,10 +4,11 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 
+
 def index(request):
 	tienda = {'name':'Tienda numero uno'}
 
 	template_name = "browser/index.html"
-	context = {'tienda':tienda}
+	context = {'tienda': tienda}
 
 	return render(request = request, template_name = template_name, context = context)
